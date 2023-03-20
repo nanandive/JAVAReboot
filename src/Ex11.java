b39b9ad5239d091438a2a7e6ac719cbc87d3884a
nanandive <diorgon12@gmail.com>
2023-03-21 05:20:30 +0900

    st10

---
+++ src/Ex11.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+
+		{
+			Scanner sc = new Scanner(System.in);
+			System.out.println("ㅅ수자를 입력하고 엔터를 치세요");
+			int num = sc.nextInt();
+			System.out.println("당신이 입력한 숫자는"+num);
+		}
+		
+		
+	}
+
+}
