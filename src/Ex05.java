f687924e9fcf88f742480e86dbaa16884d49a7c5
nanandive <diorgon12@gmail.com>
2023-11-06 05:11:50 +0900

    st4

---
+++ src/Ex05.java
@@ -0,0 +1,12 @@
+
+public class Ex05 {
+	/*
+	 * public static void main(String[] args) {
+	 * 
+	 * while (연산식) { 문장; }
+	 * 
+	 * 
+	 * 
+	 * int num =0; while(num<5) { System.out.println("홍길동"); num = num +1; } }
+	 */
+}
